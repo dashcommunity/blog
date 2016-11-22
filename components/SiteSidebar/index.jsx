ab2cdb8d92f382c91d70d9b3d73f2378ce463e40
Rion Gull <riongull@gmail.com>
2016-11-21 20:18:21 -0700

    change name and photo, revert previous gatsby-node.js edits to fix formatting (neglect terminal warning for now)

--- components/SiteSidebar/index.jsx
+++ components/SiteSidebar/index.jsx
@@ -5,7 +5,7 @@ import { config } from 'config'
 import SiteNav from '../SiteNav'
 import SiteLinks from '../SiteLinks'
 import './style.css'
-import profilePic from '../../pages/photo.jpg'
+import profilePic from '../../pages/Rion.jpeg'
 
 class SiteSidebar extends React.Component {
     render() {
