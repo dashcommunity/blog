a0026acd8e7b1cc64b5b5cd6c972e18eff638669
Rion Gull <riongull@gmail.com>
2016-11-21 19:52:08 -0700

    updated .gitignore and gatsby-node.js to remove warning, exposed public folder temporarily

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,17 +1,17 @@
-var rucksack = require('rucksack-css')
-var lost = require("lost")
-var cssnext = require("postcss-cssnext")
+// var rucksack = require('rucksack-css')
+// var lost = require("lost")
+// var cssnext = require("postcss-cssnext")
 
 exports.modifyWebpackConfig = function(config, env) {
-    config.merge({
-        postcss: [
-            lost(),
-            rucksack(),
-            cssnext({
-                browsers: ['>1%', 'last 2 versions']
-            })
-        ]
-    })
+    // config.merge({
+    //     postcss: [
+    //         lost(),
+    //         rucksack(),
+    //         cssnext({
+    //             browsers: ['>1%', 'last 2 versions']
+    //         })
+    //     ]
+    // })
 
     config.loader('svg', {
        test: /\.(svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
