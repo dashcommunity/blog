d73e67fa32bd0787b23df70f5675f29c85f8d5b0
Rion Gull <riongull@gmail.com>
2016-12-21 21:56:13 -0700

    editor communicating with GitHub

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,9 @@ var rucksack = require('rucksack-css')
 var lost = require("lost")
 var cssnext = require("postcss-cssnext")
 
+// require('dotenv').config()
+// console.log("process.env:", process.env.GH_BLOG_APP_CLIENT_ID);
+
 exports.modifyWebpackConfig = function(config, env) {
     config.merge({
         postcss: [
